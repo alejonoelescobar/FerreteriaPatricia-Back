28cc290363013a39f253c494d6765c821feddb48
alejonoel <alejoescobar2015@gmail.com>
2024-04-17 16:50:57 -0300

    cloudinary

--- src/controllers/casayjardin/createCyJ/index.js
+++ src/controllers/casayjardin/createCyJ/index.js
@@ -1,8 +1,21 @@
 import CasaYJardin from "../../../models/CasaYJardin.js";
+import multer from 'multer';
+import {v2 as cloudinary} from 'cloudinary';
+          
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const upload = multer({ dest: 'uploads/' });
 
 const createCasaYJardinItem = async (req, res) => {
   try {
+
     const { code, name, brand, cost, price, stock, classification, img } = req.body;
+    const result = await cloudinary.uploader.upload(req.file.path);
+
     const item = new CasaYJardin({
       code,
       name,
@@ -11,8 +24,11 @@ const createCasaYJardinItem = async (req, res) => {
       price,
       stock,
       classification,
+      img: result.secure_url
     });
+
     await item.save();
+
     return res.status(201).json({
       msg: "Item created successfully",
     })
